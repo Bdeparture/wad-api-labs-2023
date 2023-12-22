461b84dd41d5ad9c72d51b5e8666521895b0cadf
Sisi Chen <1599668078@qq.com>
2023-12-22 08:36:56 +0800

    added routing for posting a new tasks

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import express from 'express';
 import { tasksData } from './tasksData';
 
@@ -14,6 +15,22 @@ router.get('/:id', (req, res) => {
 
 router.get('/', (req, res) => {
     res.json(tasksData);
+});
+
+//Add a task
+router.post('/', (req, res) => {
+    const { title, description, deadline, priority, done } = req.body;
+    const newTask = {
+        id: uuidv4(),
+        title,
+        description,
+        deadline,
+        priority,
+        done
+    };
+    tasksData.tasks.push(newTask);
+    res.status(201).json(newTask);
+    tasksData.total_results++;
 });
 
 export default router;
