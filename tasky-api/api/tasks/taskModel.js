db59cd5d5bc52ad830e275b23b6df97812b85ec2
Sisi Chen <1599668078@qq.com>
2023-12-22 10:54:15 +0800

    Mongo lab Users Router

--- tasky-api/api/tasks/taskModel.js
+++ tasky-api/api/tasks/taskModel.js
@@ -3,9 +3,13 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
-  title: { type: String, required: true },
-  description:  String ,
-  deadline: Date,
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    title: { type: String, required: true },
+    description: String,
+    deadline: Date,
     done: Boolean,
     priority: { type: String, enum: ["Low", "Medium", "High"], required: true },
     created_at: Date,
@@ -14,7 +18,7 @@ const TaskSchema = new Schema({
 
 const dateValidator = (date) => {
     return date > new Date();
-  }
-  TaskSchema.path("deadline").validate(dateValidator);
-  
+}
+TaskSchema.path("deadline").validate(dateValidator);
+
 export default mongoose.model('Task', TaskSchema);
